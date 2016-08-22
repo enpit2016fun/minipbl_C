8f3ee9bf469668d8063b96c3add4e02cf5fbead7
Kazuma Osanai <2116008@fun.ac.jp>
2016-08-23 06:29:34 +0900

    Full Page Layout

--- development/kindergarten_roster_management/www/js/app.js
+++ development/kindergarten_roster_management/www/js/app.js
@@ -8,10 +8,8 @@
     });
 
     /* --------------------------------- Event Registration -------------------------------- */
-    $('.search-key').on('keyup', findByName);
-    $('.help-btn').on('click', function() {
-        alert("Employee Directory v3.4");
-    });
+      $('.search-key').on('keyup', findByName);
+
 
     /* ---------------------------------- Local Functions ---------------------------------- */
     function findByName() {
