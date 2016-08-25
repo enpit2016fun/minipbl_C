47a853c9def3c9d0e5d5b127a7240f74c35cc35f
Kazuma Osanai <Kazuma Osanai>
2016-08-25 23:34:06 +0900

    詳細画面完成。デバッグ済み。　#98　#33

--- development/kindergarten_roster_management/www/js/app.js
+++ development/kindergarten_roster_management/www/js/app.js
@@ -8,7 +8,19 @@
     });
 
     /* --------------------------------- Event Registration -------------------------------- */
+    // URLのアンカー（#以降の部分）を取得
+    var urlHash = location.hash;
+    var id_param = urlHash.replace( /#/g , "" ) ;
+
+    if(id_param){
+    // URLにアンカーが存在する場合
+      targetSerche();
+      // $('.search-key').on('keyup', targetSerche);
+
+    }else {
+    // URLにアンカーが存在しない
       $('.search-key').on('keyup', findByName);
+    }
 
     /* ---------------------------------- Local Functions ---------------------------------- */
     function findByName() {
@@ -25,17 +37,41 @@
         });
     }
 
-  // URLのアンカー（#以降の部分）を取得
-  var urlHash = location.hash;
-  var result = urlHash.replace( /#/g , "" ) ;
-  console.log(result);
-  // URLにアンカーが存在する場合
-  if(urlHash){
-    if (result == '2') {
-      service.findHello();
-      } else {
-
-      }
+    function targetSerche() {
+        service.findByName($('.search-key').val()).done(function (employees) {
+            var l = employees.length;
+            var caram;
+            if(id_param == 1){
+              caram = 1;
+            }else if (id_param == 2) {
+              caram = 0;
+            }else if (id_param == 3) {
+              caram = 10;
+            }else if (id_param == 4) {
+              caram = 7;
+            }else if (id_param == 5) {
+              caram = 5;
+            }else if (id_param == 6) {
+              caram = 4;
+            }else if (id_param == 7) {
+              caram = 2;
+            }else if (id_param == 8) {
+              caram = 6;
+            }else if (id_param == 9) {
+              caram = 8;
+            }else if (id_param == 10) {
+              caram = 11;
+            }else if (id_param == 11) {
+              caram = 9;
+            }else{
+              caram = 3;
+            }
+            console.log(caram);
+            var e = employees[caram];
+            $('.table-striped').empty();
+            $('.table-striped').append('<tr><th>ID</th><th>所属</th><th>名前</th><th>郵便番号</th><th>住所</th><th>電話番号</th><th><卒業・卒業予定></th><th>メールアドレス</th></tr>');
+            $('.table-striped').append('<tr><th>'+e.id+'</th>'+ '<th><img src="'+e.class+'" height="50px" width="50px"/></th>'+'<th>'+e.firstName+' '+e.lastName+'</th><th>'+e.post+'</th>'+'<th>'+e.place+'</th><th>'+e.cellPhone+'</th><th>'+e.guraduation+'</th><th>'+e.email+'</th></tr>');
+        });
     }
 
 }());
--- development/kindergarten_roster_management/www/js/services/websql/EmployeeService.js
+++ development/kindergarten_roster_management/www/js/services/websql/EmployeeService.js
@@ -20,17 +20,12 @@ var EmployeeService = function () {
         return deferred.promise();
     }
 
-    this.findHello = function(){
-      console.log("Hello!!!");
-    }
-
-
     this.findByName = function (searchKey) {
         var deferred = $.Deferred();
         this.db.transaction(
             function (tx) {
 
-                var sql = "SELECT e.id, e.firstName, e.lastName, e.class, e.pic, count(r.id) reportCount " +
+                var sql = "SELECT e.id, e.firstName, e.lastName, e.class, e.post, e.place, e.cellPhone, e.guraduation, e.email, count(r.id) reportCount " +
                     "FROM employee e LEFT JOIN employee r ON r.managerId = e.id " +
                     "WHERE e.firstName || ' ' || e.lastName LIKE ? " +
                     "GROUP BY e.id ORDER BY e.lastName, e.firstName";
@@ -57,7 +52,7 @@ var EmployeeService = function () {
         this.db.transaction(
             function (tx) {
 
-                var sql = "SELECT e.id, e.firstName, e.lastName, e.class, e.city, e.officePhone, e.cellPhone, e.email, e.pic, e.managerId, m.firstName managerFirstName, m.lastName managerLastName, count(r.id) reportCount " +
+                var sql = "SELECT e.id, e.firstName, e.lastName, e.class, e.guraduation, e.post, e.cellPhone, e.email, e.place, e.managerId, m.firstName managerFirstName, m.lastName managerLastName, count(r.id) reportCount " +
                     "FROM employee e " +
                     "LEFT JOIN employee r ON r.managerId = e.id " +
                     "LEFT JOIN employee m ON e.managerId = m.id " +
@@ -82,10 +77,10 @@ var EmployeeService = function () {
             "lastName VARCHAR(50), " +
             "class VARCHAR(50), " +
             "managerId INTEGER, " +
-            "city VARCHAR(50), " +
-            "officePhone VARCHAR(50), " +
+            "guraduation VARCHAR(50), " +
+            "post VARCHAR(50), " +
             "cellPhone VARCHAR(50), " +
-            "pic VARCHAR(50), " +
+            "place VARCHAR(50), " +
             "email VARCHAR(50))";
         tx.executeSql(sql, null,
             function () {
@@ -99,27 +94,27 @@ var EmployeeService = function () {
     var addSampleData = function (tx, employees) {
 
         var employees = [
-            {"id": 1, "firstName": "田中", "lastName": "太郎", "managerId": 0, "managerName": "", "class": "img/bird.png", "department": "Corporate", "cellPhone": "617-000-0001", "officePhone": "781-000-0001", "email": "jking@fakemail.com", "city":"Boston,MA","pic":"James_King.jpg","twitterId":"@fakejking","blog":"http://coenraets.org"},
-            {"id": 2, "firstName": "中島", "lastName": "佑都", "managerId": 1, "managerName": "James King", "class": "img/bird.png", "department": "Marketing", "cellPhone": "617-000-0002", "officePhone": "781-000-0002", "email": "jtaylor@fakemail.com", "city": "Boston, MA", "pic": "Julie_Taylor.jpg", "twitterId": "@fakejtaylor", "blog": "http://coenraets.org"},
-            {"id": 3, "firstName": "鍋島", "lastName": "隆", "managerId": 1, "managerName": "James King", "class": "img/zou.png", "departmen": "Accounting", "cellPhone": "617-000-0003", "officePhone": "781-000-0003", "email": "elee@fakemail.com", "city": "Boston, MA", "pic": "Eugene_Lee.jpg", "twitterId": "@fakeelee", "blog": "http://coenraets.org"},
-            {"id": 4, "firstName": "佐々木", "lastName": "華", "managerId": 1, "managerName": "James King", "class": "img/zou.png", "department": "Engineering", "cellPhone": "617-000-0004", "officePhone": "781-000-0004", "email": "jwilliams@fakemail.com", "city": "Boston, MA", "pic": "John_Williams.jpg", "twitterId": "@fakejwilliams", "blog": "http://coenraets.org"},
-            {"id": 5, "firstName": "高橋", "lastName": "由紀子", "managerId": 1, "managerName": "James King", "class": "img/bird.png", "department": "Sales", "cellPhone": "617-000-0005", "officePhone": "781-000-0005", "email": "rmoore@fakemail.com", "city": "Boston, MA", "pic": "Ray_Moore.jpg", "twitterId": "@fakermoore", "blog": "http://coenraets.org"},
-            {"id": 6, "firstName": "木村", "lastName": "瑞樹", "managerId": 4, "managerName": "John Williams", "class": "img/rabit.png", "department": "Engineering", "cellPhone": "617-000-0006", "officePhone": "781-000-0006", "email": "pjones@fakemail.com", "city": "Boston, MA", "pic": "Paul_Jones.jpg", "twitterId": "@fakepjones", "blog": "http://coenraets.org"},
-            {"id": 7, "firstName": "佐藤", "lastName": "武志", "managerId": 4, "managerName": "John Williams", "class": "img/kujira.png", "department": "Engineering", "cellPhone": "617-000-0007", "officePhone": "781-000-0007", "email": "pgates@fakemail.com", "city": "Boston, MA", "pic": "Paula_Gates.jpg", "twitterId": "@fakepgates", "blog": "http://coenraets.org"},
-            {"id": 8, "firstName": "桜木", "lastName": "花道", "managerId": 2, "managerName": "Julie Taylor", "class": "img/dolphin.png", "department": "Marketing", "cellPhone": "617-000-0008", "officePhone": "781-000-0008", "email": "lwong@fakemail.com", "city": "Boston, MA", "pic": "Lisa_Wong.jpg", "twitterId": "@fakelwong", "blog": "http://coenraets.org"},
-            {"id": 9, "firstName": "橋本", "lastName": "萌花", "managerId": 2, "managerName": "Julie Taylor", "class": "img/hippo.png", "department": "Marketing", "cellPhone": "617-000-0009", "officePhone": "781-000-0009", "email": "gdonovan@fakemail.com", "city": "Boston, MA", "pic": "Gary_Donovan.jpg", "twitterId": "@fakegdonovan", "blog": "http://coenraets.org"},
-            {"id": 10, "firstName": "清水", "lastName": "雄二", "managerId": 5, "managerName": "Ray Moore", "class": "img/hippo.png", "department": "Sales", "cellPhone": "617-000-0010", "officePhone": "781-000-0010", "email": "kbyrne@fakemail.com", "city": "Boston, MA", "pic": "Kathleen_Byrne.jpg", "twitterId": "@fakekbyrne", "blog": "http://coenraets.org"},
-            {"id": 11, "firstName": "吉岡", "lastName": "里見", "managerId": 5, "managerName": "Ray Moore", "class": "img/dolphin.png", "department": "Sales", "cellPhone": "617-000-0011", "officePhone": "781-000-0011", "email": "ajones@fakemail.com", "city": "Boston, MA", "pic": "Amy_Jones.jpg", "twitterId": "@fakeajones", "blog": "http://coenraets.org"},
-            {"id": 12, "firstName": "坂本", "lastName": "燐火", "managerId": 4, "managerName": "John Williams", "class": "img/hippo.png", "department": "Engineering", "cellPhone": "617-000-0012", "officePhone": "781-000-0012", "email": "swells@fakemail.com", "city": "Boston, MA", "pic": "Steven_Wells.jpg", "twitterId": "@fakeswells", "blog": "http://coenraets.org"}
+            {"id": 1, "firstName": "田中", "lastName": "太郎", "managerId": 1, "managerName": "", "class": "img/bird.png", "place": "北海道函館市大縄町14-14", "cellPhone": "017-739-1111", "post": "041-0811", "email": "jking@fakemail.com", "guraduation":"2017","twitterId":"@fakejking","blog":"http://coenraets.org"},
+            {"id": 2, "firstName": "中島", "lastName": "佑都", "managerId": 2, "managerName": "James King", "class": "img/bird.png", "place": "北海道函館市大縄町14-13", "cellPhone": "017-739-1112", "post": "041-0811", "email": "jtaylor@fakemail.com", "guraduation": "2018", "twitterId": "@fakejtaylor", "blog": "http://coenraets.org"},
+            {"id": 3, "firstName": "鍋島", "lastName": "隆", "managerId": 3, "managerName": "James King", "class": "img/zou.png", "place": "北海道函館市大縄町14-12", "cellPhone": "017-739-1113", "post": "041-0811", "email": "elee@fakemail.com", "guraduation": "2019", "twitterId": "@fakeelee", "blog": "http://coenraets.org"},
+            {"id": 4, "firstName": "佐々木", "lastName": "華", "managerId": 4, "managerName": "James King", "class": "img/zou.png", "place": "北海道函館市大縄町14-11", "cellPhone": "017-739-1114", "post": "041-0811", "email": "jwilliams@fakemail.com", "guraduation": "2020", "twitterId": "@fakejwilliams", "blog": "http://coenraets.org"},
+            {"id": 5, "firstName": "高橋", "lastName": "由紀子", "managerId": 5, "managerName": "James King", "class": "img/bird.png", "place": "青森県青森市高田字朝日山809-221", "cellPhone": "017-739-1111", "post": "030-0151", "email": "rmoore@fakemail.com", "guraduation": "2008", "twitterId": "@fakermoore", "blog": "http://coenraets.org"},
+            {"id": 6, "firstName": "木村", "lastName": "瑞樹", "managerId": 6, "managerName": "John Williams", "class": "img/rabit.png", "place": "北海道北斗市中野通300", "cellPhone": "017-739-2222", "post": "049-0156", "email": "pjones@fakemail.com", "guraduation": "2009", "twitterId": "@fakepjones", "blog": "http://coenraets.org"},
+            {"id": 7, "firstName": "佐藤", "lastName": "武志", "managerId": 7, "managerName": "John Williams", "class": "img/kujira.png", "place": "北海道北斗市中野通200", "cellPhone": "017-739-3333", "post": "049-0156", "email": "pgates@fakemail.com", "guraduation": "2009, MA", "twitterId": "@fakepgates", "blog": "http://coenraets.org"},
+            {"id": 8, "firstName": "桜木", "lastName": "花道", "managerId": 2, "managerName": "Julie Taylor", "class": "img/dolphin.png", "place": "青森県青森市荒川", "cellPhone": "017-739-4444", "post": "030-0011", "email": "lwong@fakemail.com", "guraduation": "2008", "twitterId": "@fakelwong", "blog": "http://coenraets.org"},
+            {"id": 9, "firstName": "橋本", "lastName": "萌花", "managerId": 8, "managerName": "Julie Taylor", "class": "img/hippo.png", "place": "青森県青森市油川", "cellPhone": "017-739-5555", "post": "030-0201", "email": "gdonovan@fakemail.com", "guraduation": "2014", "twitterId": "@fakegdonovan", "blog": "http://coenraets.org"},
+            {"id": 10, "firstName": "清水", "lastName": "雄二", "managerId": 9, "managerName": "Ray Moore", "class": "img/hippo.png", "place": "青森県青森市大野", "cellPhone": "017-739-6666", "post": "030-0338", "email": "kbyrne@fakemail.com", "guraduation": "2015", "twitterId": "@fakekbyrne", "blog": "http://coenraets.org"},
+            {"id": 11, "firstName": "吉岡", "lastName": "里見", "managerId": 10, "managerName": "Ray Moore", "class": "img/dolphin.png", "place": "青森県青森市甲田", "cellPhone": "017-739-7777", "post": "030-0667", "email": "ajones@fakemail.com", "guraduation": "2017", "twitterId": "@fakeajones", "blog": "http://coenraets.org"},
+            {"id": 12, "firstName": "坂本", "lastName": "燐火", "managerId": 11, "managerName": "John Williams", "class": "img/hippo.png", "place": "北海道函館市富岡町2-45", "cellPhone": "017-739-8888", "post": "041-0880", "email": "swells@fakemail.com", "guraduation": "2019","twitterId": "@fakeswells", "blog": "http://coenraets.org"}
         ];
         var l = employees.length;
         var sql = "INSERT OR REPLACE INTO employee " +
-            "(id, firstName, lastName, managerId, class, city, officePhone, cellPhone, email, pic) " +
+            "(id, firstName, lastName, managerId, class, guraduation, post, cellPhone, email, place) " +
             "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
         var e;
         for (var i = 0; i < l; i++) {
             e = employees[i];
-            tx.executeSql(sql, [e.id, e.firstName, e.lastName, e.managerId, e.class, e.city, e.officePhone, e.cellPhone, e.email, e.pic],
+            tx.executeSql(sql, [e.id, e.firstName, e.lastName, e.managerId, e.class, e.guraduation, e.post, e.cellPhone, e.email, e.place],
                 function () {
                     console.log('INSERT success');
                 },
